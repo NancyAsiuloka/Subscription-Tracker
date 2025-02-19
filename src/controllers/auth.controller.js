5c8c7537ee11f9acaa6cad54e9828b212c037dc1
Nancy Asiuloka <nancyasiuloka@gmail.com>
2025-02-19 08:38:12 +0100

    made some changes

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 import User from "../models/user.model.js";
-import bcrypt from "bcryptjs"
-import jwt from "jsonwebtoken"
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
 import { JWT_SECRET, JWT_EXPIRES_IN } from "../../config/env.js";
 
 export const signUp = async (req, res, next) => {
@@ -13,7 +13,7 @@ export const signUp = async (req, res, next) => {
     const { name, email, password } = req.body;
 
     // Check if the user already exists
-    const existingUser = await User.findOne({email})
+    const existingUser = await User.findOne({ email });
 
     if (existingUser) {
       const error = new Error("User with this email already exists");
@@ -25,10 +25,25 @@ export const signUp = async (req, res, next) => {
     const salt = await bcrypt.genSalt(10);
     const hashPassword = await bcrypt.hash(password, salt);
 
-    const newUsers = await User.create([{name, email, password: hashPassword}], { session });
-    const token = jwt.sign({userId: newUsers[0]._id}, JWT_SECRET, { expiresIn: JWT_EXPIRES_IN})
+    const newUsers = await User.create(
+      [{ name, email, password: hashPassword }],
+      { session }
+    );
+    const token = jwt.sign({ userId: newUsers[0]._id }, JWT_SECRET, {
+      expiresIn: JWT_EXPIRES_IN,
+    });
 
     await session.commitTransaction(); // commit the transaction
+    session.endSession();
+
+    res.status(201).json({
+      success: true,
+      message: "User created",
+      data: {
+        token,
+        user: newUsers[0],
+      },
+    });
   } catch (error) {
     await session.abortTransaction(); // abort the transaction
     session.endSession(); // end the session
