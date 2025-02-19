34e4dcc3df8f907f9b42b97fba6055ecb1e9d8b1
Nancy Asiuloka <nancyasiuloka@gmail.com>
2025-02-19 08:23:34 +0100

    Added sign up endpoint

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,4 +1,6 @@
 import mongoose from "mongoose";
+import User from "../models/user.model.js";
+import bcrypt from "bcryptjs"
 
 export const signUp = async (req, res, next) => {
   const session = await mongoose.startSession(); // is a session of a mongoose transaction
@@ -6,6 +8,16 @@ export const signUp = async (req, res, next) => {
 
   try {
     // Logic to create a new user
+    const { name, email, password } = req.body;
+    const existingUser = await User.findOne({email})
+    if (existingUser) {
+      const error = new Error("User with this email already exists");
+      error.statusCode = 409;
+      throw error;
+    }
+
+    // Hash the password
+    const salt = await bcrypt.genSalt(10);
 
     await session.commitTransaction(); // commit the transaction
   } catch (error) {
