8f662b1646ce5df9d3db7ba1b08c8ebd30f7d030
Nancy Asiuloka <nancyasiuloka@gmail.com>
2025-02-19 08:31:45 +0100

    Created a token for the new user

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import mongoose from "mongoose";
 import User from "../models/user.model.js";
 import bcrypt from "bcryptjs"
+import jwt from "jsonwebtoken"
 
 export const signUp = async (req, res, next) => {
   const session = await mongoose.startSession(); // is a session of a mongoose transaction
@@ -23,8 +24,8 @@ export const signUp = async (req, res, next) => {
     const salt = await bcrypt.genSalt(10);
     const hashPassword = await bcrypt.hash(password, salt);
 
-    const newUser = await User.create([{name, email, password: hashPassword}], { session });
-
+    const newUsers = await User.create([{name, email, password: hashPassword}], { session });
+    const token = jwt.sign({userId: newUsers[0]._id}, JWT_SECRET, { expiresIn: 1hr})
 
     await session.commitTransaction(); // commit the transaction
   } catch (error) {
