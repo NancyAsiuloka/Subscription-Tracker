310f33b101f14f486d9ff733e724c9ab9adb55a7
Nancy Asiuloka <nancyasiuloka@gmail.com>
2025-02-27 11:32:55 +0100

    added getUsers endpoints to fetch all users

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,12 +1,17 @@
 import User from "../models/user.model";
 
-export const getUsers = async (req, resizeBy, next) => {
-    try {
-        const users = await User.find();
-        return {
-            users
-        }
-    } catch (error) {
-        next(error)
-    }
-}
+export const getUsers = async (req, res, next) => {
+  try {
+    const users = await User.find();
+
+    res.status(200).json({
+      success: true,
+      message: "Users found",
+      data: {
+        users,
+      },
+    });
+  } catch (error) {
+    next(error);
+  }
+};
