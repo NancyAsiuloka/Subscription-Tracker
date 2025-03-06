04b82947e4214307b8dc6c833fb66c818660b720
Nancy Asiuloka <nancyasiuloka@gmail.com>
2025-03-06 11:37:49 +0100

    returns all the  users

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -6,9 +6,7 @@ export const getUsers = async (req, res, next) => {
 
     res.status(200).json({
       success: true,
-      data: {
-        users,
-
+      data: users,
     });
   } catch (error) {
     next(error);
