d5d07b20cd8729732099c45513c237d17b9bfd85
Nancy Asiuloka <nancyasiuloka@gmail.com>
2025-03-12 11:55:16 +0100

    added the users route for the endpoints created

--- src/routes/user.router.js
+++ src/routes/user.router.js
@@ -1,12 +1,12 @@
 import { Router } from "express";
+import { getUser, getUsers } from "../controllers/user.controller";
 
 const userRouter = Router();
 
-userRouter.get('/', (req, res) => res.send('Fetch all users'));
-userRouter.get('/:id', (req, res) => res.send('Fetch user by id'));
-userRouter.post('/', (req, res) => res.send('Create user'));
-userRouter.put('/:id', (req, res) => res.send('Update user by id'));
-userRouter.delete('/:id', (req, res) => res.send('Delete user by id'));
-
+userRouter.get("/", getUsers);
+userRouter.get("/:id", getUser);
+userRouter.post("/", (req, res) => res.send("Create user"));
+userRouter.put("/:id", (req, res) => res.send("Update user by id"));
+userRouter.delete("/:id", (req, res) => res.send("Delete user by id"));
 
 export default userRouter;
