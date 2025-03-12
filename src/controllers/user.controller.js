3bb10ab390e5be245037d8d53a7c0ed2b7fef81b
Nancy Asiuloka <nancyasiuloka@gmail.com>
2025-03-12 11:52:19 +0100

    added error message for user if not found

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -17,6 +17,11 @@ export const getUser = async (req, res, next) => {
   try {
     const user = await User.findOne({ _id: req.params.id }).select("-password");
 
+    if (!user) {
+      const error = new Error("User not found");
+      error.statusCode = 404;
+      throw error;
+    }
     res.status(200).json({
       success: true,
       data: user,
