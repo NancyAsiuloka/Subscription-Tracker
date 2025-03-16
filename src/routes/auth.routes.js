498fe1f07efc7483a2f421f1130b68ad01c08ee6
Nancy Asiuloka <nancyasiuloka@gmail.com>
2025-03-16 10:33:56 +0100

    added the signout function

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -11,7 +11,7 @@ const authRouter = Router();
 
 authRouter.post("/sign-up", signUp);
 authRouter.post("/sign-in", signIn);
-authRouter.post("/sign-out", signUp);
+authRouter.post("/sign-out", signOut);
 authRouter.post("/update-password", updatePassword);
 authRouter.post("/forgot-password", forgotPassword);
 authRouter.post("/reset-password", resetPassword);
