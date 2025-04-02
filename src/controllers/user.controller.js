7d883d300b7088782d1884a1dcb5de4673094fc2
Nancy Asiuloka <nancyasiuloka@gmail.com>
2025-04-02 09:56:38 +0100

    Adjusted the message content

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -5,6 +5,7 @@ export const getUsers = async (req, res, next) => {
     const users = await User.find();
 
     res.status(200).json({
+      message: "User created Successfuly",
       success: true,
       data: users,
     });
