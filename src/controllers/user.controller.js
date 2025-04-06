9e74c059bef277a37f53339d00d8ce9793dd492f
Nancy Asiuloka <nancyasiuloka@gmail.com>
2025-04-06 09:53:49 +0100

    made some changes

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -36,7 +36,7 @@ export const createUser = async (req, res, next) => {
     const user = await User.create(req.body);
 
     res.status(200).json({
-      message: "",
+      message: "User created Successfuly",
       sucess: true,
       data: user,
     });
