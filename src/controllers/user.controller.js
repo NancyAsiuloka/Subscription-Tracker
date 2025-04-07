cb47ac82a68abc1b1c03f6edbe546d7a7d3db7f4
Nancy Asiuloka <nancyasiuloka@gmail.com>
2025-04-07 09:53:18 +0100

    added a message on succesful operation

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -36,6 +36,7 @@ export const createUser = async (req, res, next) => {
     const user = await User.create(req.body);
 
     res.status(200).json({
+      message: "",
       sucess: true,
       data: user,
     });
