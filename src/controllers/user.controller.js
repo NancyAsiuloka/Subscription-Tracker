6e3e5e6730013bc229bfc341c9a7f523a76f1423
Nancy Asiuloka <nancyasiuloka@gmail.com>
2025-04-12 09:49:31 +0100

    Added a comment

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -69,13 +69,13 @@ export const updateUser = async (req, res, next) => {
 
 export const deleteUser = async (req, res, next) => {
   try {
-    const user = await User.findOneAndUpdate({ _id: req.params.id });
+    const user = await User.findOneAndDelete({ _id: req.params.id });
 
-    if (!user) {
-      const error = new Error("User not found");
-      error.statusCode = 404;
-      throw error;
-    }
+    // if (!user) {
+    //   const error = new Error("User not found");
+    //   error.statusCode = 404;
+    //   throw error;
+    // }
     res.status(200).json({
       message: "User updated successfuly",
       success: true,
