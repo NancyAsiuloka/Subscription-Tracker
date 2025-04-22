981826c4b2f770c391bfa9768bb17d78bea1e055
Nancy Asiuloka <nancyasiuloka@gmail.com>
2025-04-22 09:32:30 +0100

    Added the update user endpoint

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -43,3 +43,21 @@ export const createUser = async (req, res, next) => {
     next(error.message);
   }
 };
+
+export const updateUser = async (req, res, next) => {
+  try {
+    const user = await User.findOne({ _id: req.params.id });
+
+    if (!user) {
+      const error = new Error("User not found");
+      error.statusCode = 404;
+      throw error;
+    }
+    res.status(200).json({
+      success: true,
+      data: user,
+    });
+  } catch (error) {
+    next(error.message);
+  }
+};
