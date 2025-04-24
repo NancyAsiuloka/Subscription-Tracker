caf4288a8d4fa7f39e445554363d344c32aae34d
Nancy Asiuloka <nancyasiuloka@gmail.com>
2025-04-24 09:29:54 +0100

    added the post route for creating a user

--- src/routes/user.router.js
+++ src/routes/user.router.js
@@ -5,7 +5,7 @@ const userRouter = Router();
 
 userRouter.get("/", getUsers);
 userRouter.get("/:id", getUser);
-userRouter.post("/", (req, res) => res.send("Create user"));
+userRouter.post("/", createUser);
 userRouter.put("/:id", (req, res) => res.send("Update user by id"));
 userRouter.delete("/:id", (req, res) => res.send("Delete user by id"));
 
