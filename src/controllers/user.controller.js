4d84cd0faf1bdb45339170d5bc16b1b6e13f3c17
Nancy Asiuloka <nancyasiuloka@gmail.com>
2025-04-25 09:28:36 +0100

    Rapped the function in a try-catch block

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -32,10 +32,14 @@ export const getUser = async (req, res, next) => {
 };
 
 export const createUser = async (req, res, next) => {
-  const user = await User.create(req.body);
+  try {
+    const user = await User.create(req.body);
 
-  res.status(200).json({
-    sucess: true,
-    data: user,
-  });
+    res.status(200).json({
+      sucess: true,
+      data: user,
+    });
+  } catch (error) {
+    next(error.message);
+  }
 };
