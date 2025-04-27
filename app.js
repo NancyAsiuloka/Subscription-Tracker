f65b60d4c8217406f883624f5e6227c802085f8f
Nancy Asiuloka <nancyasiuloka@gmail.com>
2025-04-28 00:21:54 +0100

    added the arcjet middleware in the app.js file

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ import userRouter from "./src/routes/user.router.js";
 import SubscriptionRouter from "./src/routes/subscription.routes.js";
 import connectDB from "./database/mongodb.js";
 import errorMiddleware from "./middlewares/error.middleware.js";
+import { arcjetMiddleware } from "./middlewares/arcjet.middleware.js";
 
 const app = express();
 
@@ -13,6 +14,7 @@ app.use(express.json());
 // help to process the fromdata sent by html forms in a simple format
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser()); //reads cookies from incoming requests so app can store user data
+app.use(arcjetMiddleware());
 
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/users", userRouter);
